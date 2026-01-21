6a0c416b7761ddf38df70caab6860ac385de5199
Abderrazzak Hmidat <abhmidat@e2r9p6.1337.ma>
2026-01-21 15:43:42 +0100

    last one

---
+++ cppModule04/ex00/Animal.hpp
@@ -0,0 +1,22 @@
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+#include <string>
+
+class Animal {
+	protected :
+		std::string type;
+
+};
+
+class Dog : public Animal {
+
+
+};
+
+class Cat : public Animal {
+
+};
+
+#endif
